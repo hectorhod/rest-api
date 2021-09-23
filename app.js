c53483fc5657a708b96abea9db3a23db8853bfff
Hectorhod <hectorfreitas@hotmail.com>
2021-09-23 17:48:36 -0300

    Implementado comunicacao com o Banco de Dados e CRUD

--- app.js
+++ app.js
@@ -1,30 +1,70 @@
 var express = require("express");
 var app = express();
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+const { ObjectID } = require("bson");
 
-app.get("/url", (req, res, next) =>
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+const uri = "mongodb+srv://dbUser:userDB1234@cluster0.qpj9v.mongodb.net/dadosdb?retryWrites=true&w=majority";
+
+MongoClient.connect(uri, (err, client) => {
+    if (err) return console.log(err)
+    db = client.db('dadosdb') //Nome do Banco de Dados
+
+    app.listen(3000, () => {
+        console.log("Servidor rodando na porta 3000");
+    });
+})
+
+app.get("/", (req, res, next) =>
 {
-    res.json(["Julio", "Giba", "Xandão", "Miltinho"])
+    db.collection('users').find().toArray((err, results) => {
+        if (err) return console.log(err)
+        res.json(results)
+        console.log(results);
+    })
 });
 
-app.put('/put/:id', (req,res) => 
+app.post('/update/:id', (req,res) => 
 {
-    console.log("chegou no PUT...");
-    res.status(200).send('retorno do PUT');
+    var id = req.params.id
+    var username = req.body.username
+    var email = req.body.email
+    const ObjectID = require('mongodb').ObjectId
+
+    db.collection('users').updateOne({_id:ObjectID(id)}, {
+        $set: {
+            username: username,
+            email: email
+        }
+    }, (err, result) => {
+        if (err) return res.send(err)
+        res.status(200).json("Usuário alterado")
+    })
+    
+    
 });
 
-app.post('/post', (req, res) =>
+app.post('/create', (req, res) =>
 {
-    console.log("chegou no POST...");
-    res.status(200).json(["Programação", "API", "JSON"]);
+    const user = req.body
+    db.collection('users').insertOne(user, (err, result) => {
+        if (err) return res.send(err)
+
+        res.status(200).json('Usuário criado com sucesso!')
+    })
 });
+
 
 app.delete('/delete/:id', (req, res) =>
 {
-    console.log("chegou no DELETE...");
-    res.status(200).send('utilização correta do DELETE para excluir');
-});
+    var id = req.params.id
 
-app.listen(3000, () =>
-{
-    console.log("Servidor rodando na porta 3000");
+    db.collection('users').deleteOne({_id: ObjectID(id)}, (err, result) => {
+        if (err) return res.send(500, err)
+        res.status(200).json('Deletado do Banco de dados!');
+        console.log('Deletado do Banco de dados!');
+    })
 });
